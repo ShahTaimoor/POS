6e78b83cc8812a6c36e398527533ea00bee45a71
ShahTaimoor <taimour448@gmail.com>
2025-06-27 16:41:16 +0500

    S

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,7 +60,7 @@ const Home = () => {
     </div>
 
     {/* Stats Grid */}
-    <div className="grid sm:w-[700px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid sm:w-[700px] grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {stats.map((stat, index) => (
         <div
           key={index}
